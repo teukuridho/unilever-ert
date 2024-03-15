0afa9e46005fb923dcf667e02e156cd16ce503d1
rhysqi <rhysqi.akbar@gmail.com>
2024-03-15 19:21:44 +0700

    Adding main content

--- src/app/components/slides/Slides3.tsx
+++ src/app/components/slides/Slides3.tsx
@@ -1,5 +1,19 @@
+import "../../globals.css"
+
+function Content(){
+    return(
+        <div className="bg-white h-1/3 font-sans mb-3 flex flex-row">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat 
+            consequuntur est sed. Sit vel atque moll
+        </div>
+    )
+}
+
 export default function Slide3() {
     return (
-        <div>Ini merupakan slide 3</div>
+        <div className="bg-blue-900 h-screen">
+            <Content></Content>
+            <Content></Content>
+        </div>
     )
 }
